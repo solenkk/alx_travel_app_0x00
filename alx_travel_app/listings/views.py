3e179cab4c2bdd6c922657ed9f300580404611bb
solen <kassahunsolen@gmail.com>
2025-07-20 19:35:57 +0300

    Create views.py

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Example:
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+#
+# class ListingList(APIView):
+#     def get(self, request):
+#         return Response({"message": "Hello from listings!"})
